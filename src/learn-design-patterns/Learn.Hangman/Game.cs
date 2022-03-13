16ac76dc9292c68efe1e45354fd3247979b0971d
SeferMirza <sefermirza@gmail.com>
2022-03-13 20:21:10 +0300

    Adam asma, adamın her durumu yapıldı ve döndürüldü

---
+++ src/learn-design-patterns/Learn.Hangman/Game.cs
@@ -0,0 +1,40 @@
+﻿using Learn.Hangman.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Learn.Hangman
+{
+    public class Game
+    {
+        public void Start()
+        {
+            Man man = new Man();
+            User player1 = new User("Ben");
+            User player2 = new User("Sen");
+            int round = 0;
+            int failCount = 0;
+            while (true)
+            {
+                if(round % 2 == 0)
+                {
+                    WriteLine("-->" + player1.Name + "     " + player2.Name);
+                }
+                else
+                {
+                    WriteLine(player1.Name + "     " + "-->" + player2.Name);
+                }
+                
+                WriteLine(man.GetMan(failCount));
+                ConsoleKeyInfo keyPressed = ReadKey();
+                if (keyPressed.Key == ConsoleKey.RightArrow)
+                {
+                    failCount++;
+                }
+                round++;
+            }
+        }
+    }
+}
---
+++ src/learn-design-patterns/Learn.Hangman/Interface/IHangman.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Hangman.Interface
+{
+    public interface IHangman
+    {
+        string GetWord();
+        string GetHangman(int state);
+    }
+}
