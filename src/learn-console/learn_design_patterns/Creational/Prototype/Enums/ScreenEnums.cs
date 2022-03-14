86903084d77dd3e77eb3eacec077bcff66a5a44e
SeferMirza <sefermirza@gmail.com>
2022-03-14 12:44:38 +0300

    Yeni enums folder açıldı ve enumlar oraya taşındı

---
+++ src/learn-console/learn_design_patterns/Creational/Prototype/Enums/ScreenEnums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.DesignPatterns.Creational.Prototype.Enums
+{
+    enum screenResolution
+    {
+        Low,
+        Medium,
+        High,
+        GTX3090
+    }
+}
