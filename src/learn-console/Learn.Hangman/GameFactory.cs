11c02580f1f57dba0395bb3747dc8f2dbfeb58d5
SeferMirza <sefermirza@gmail.com>
2022-03-23 18:01:35 +0300

    default value update

--- src/learn-console/Learn.Hangman/GameFactory.cs
+++ src/learn-console/Learn.Hangman/GameFactory.cs
@@ -7,7 +7,7 @@ namespace Learn.Hangman
     {
         public Game CreateDefault() =>
             new Game("I AM IRONMAN",
-                maxGuesses: 4,
+                maxGuesses: 9,
                 text: new MixedText(
                     new EliteText(),
                     new BloodyText()
